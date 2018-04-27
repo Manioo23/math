87558ab1e16cb7655464ec9fb04cf4341a6ab599
karylsienn <niewiadomski.k@wp.pl>
2018-04-28 00:26:04 +0200

    cos

--- D3Perceptron/moving_circle.js
+++ D3Perceptron/moving_circle.js
@@ -9,11 +9,19 @@ svg	.append('g')
 	.attr('class', 'circle');
 
 
-d3	.select('.circle')
+	
+
+
+d3  .select('.circle')
 	.transition()
 	.attr('cx', new_center.x)
 	.attr('cy', new_center.y)
 	.attr('r', new_radius)
-	.duration(1000);
+	.duration(10000);
 
-	
+d3	.selectAll('.data_points')
+	.attr('fill', d => { 
+		var shit = ( (d.attr('cx') - d3.select('.circle').attr('cx'))**2 + (d.attr('cy') - d3.select('.circle').attr('cy'))**2 <= d3.select('.circle').attr('r')**2);
+		console.log(shit);
+		return shit ? 'green' : 'red';
+	 } );
