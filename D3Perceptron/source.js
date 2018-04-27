1f98b206f9424bfd0e4b5901cbbddedb3a56c5ff
Maciej Witkowski <maciekwitkowski322@gamil.com>
2018-04-27 23:26:54 +0200

    Add final circle

--- D3Perceptron/source.js
+++ D3Perceptron/source.js
@@ -9,13 +9,26 @@ let randomX = d3.randomUniform(margin, width - margin),
     randomY = d3.randomUniform(height - margin, margin),
     data = [];
 
+let finalCircle = {
+    x: randomX(),
+    y: randomY(),
+    r: 100
+}
+
 class Point {
     constructor(_x, _y) {
         this.x = _x;
         this.y = _y;
+        this.isInCircle = (this.x - finalCircle.x) ** 2 + (this.y - finalCircle.y) ** 2 <= finalCircle.r ** 2; 
     }
 }
 
 var svg = d3.select('body').append('svg')
 	.attr('width', width)
-	.attr('height', height);
+    .attr('height', height);
+    
+svg .append('circle')
+    .attr('cx', finalCircle.x)
+    .attr('cy', finalCircle.y)
+    .attr('r', finalCircle.r)
+    .style('stroke', 'white');
