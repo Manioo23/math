04918c1ded3f07a99c9f65ee7c56c7fad070e537
Maciej Witkowski <maciekwitkowski322@gamil.com>
2018-04-28 00:22:36 +0200

    Addet transition effect

--- D3Perceptron/moving_circle.js
+++ D3Perceptron/moving_circle.js
@@ -8,12 +8,27 @@ svg	.append('g')
 	.attr('r', radius)
 	.attr('class', 'circle');
 
+let my_circle = d3.select('.circle');
 
-d3	.select('.circle')
+
+my_circle
 	.transition()
 	.attr('cx', new_center.x)
 	.attr('cy', new_center.y)
 	.attr('r', new_radius)
-	.duration(1000);
+	.duration(10000);
 
-	
+let checkData = () => {
+	let tempX = my_circle.attr('cx'),
+		tempY = my_circle.attr('cy'),
+		tempR = my_circle.attr('r'),
+		tempGuess = null;
+
+	d3	.selectAll('.dataPoints')
+		.style('fill', d => {
+			tempGuess = (d.x - tempX) ** 2 + (d.y - tempY) ** 2 <= tempR ** 2;
+			return tempGuess ? 'green' : 'red';
+		})
+}
+
+setInterval(() => checkData() , 5);
