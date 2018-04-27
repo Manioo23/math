8280c7e74d9a80cb8043aa84c9bf0af87edb0fbe
Maciej Witkowski <maciekwitkowski322@gamil.com>
2018-04-27 22:22:18 +0200

    Add data_init file

---
+++ D3Perceptron/data_init.js
@@ -0,0 +1,12 @@
+for(let i = 0; i < DATA_COUNT; i++) {
+    data.push(new Point(randomX(), randomY()))
+}
+
+svg .selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('cx', d => { return d.x })
+	.attr('cy', d => { return d.y })
+	.attr('r', 5)
+    .style('fill', 'white')
