4da4864fe6012e0410c54e6bae2e940641cbb970
Maciej Witkowski <maciekwitkowski322@gamil.com>
2018-05-08 10:58:26 +0200

    Syntax changes

--- D3Perceptron/perceptron.js
+++ D3Perceptron/perceptron.js
@@ -7,51 +7,43 @@ class Perceptron {
         y: 0
     }
     
-    constructor(_x = null, _y = null, _radius = null, _lr = null) {
-        this.pos.x = _x | randomX();
-        this.pos.y = _y | randomY();
-        this.radius = _radius | d3.randomUniform(10, 800)();
+    constructor(_x = randomX(), _y = randomY(), _radius = d3.randomUniform(10, 800)(), _lr = null) {
+        this.pos.x = _x;
+        this.pos.y = _y;
+        this.radius = _radius;
         this.learning_rate = _lr | 0.001;
     }
 
     train = (inputs) => {
-        /** Learning */
+        /** Learning based on given Array of inputs */
         inputs.forEach(element => {
             let x = element.x,
                 y = element.y,
                 isInCircle = element.isInCircle;
             
+            /** Calculating error */
             let predict = activation(x, y);
             let error = predict - isInCircle;
 
             this.radius += error * _lr;
             this.pos.x -= error * _lr;
             this.pos.y -= error * _lr;
-
-
-
         });
 
     }
 
     activation = (x, y) => {
-        /** Activation Function */
+        /** left and right sides of cricle equation */
         let left = (x - this.pos.x) ** 2 + (y - this.pos.y) ** 2,
             right = this.radius ** 2;
         return left <= right ? 1 : 0;
     }
 
-<<<<<<< HEAD
     move = (newX, newY) => {
         d3  .select('.circle')
             .transition()
             .attr('cx', newX)
             .attr('cy', newY)
             .duration(1000);
-=======
-    move = () => {
-       /** Moving Circle DOM Element */
-       
->>>>>>> 90fc06751cde70c49c5791755c52981b75d06130
     }
 }
