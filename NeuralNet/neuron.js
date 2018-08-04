18720eb687e7037ad914c030309e9b2903ae88e0
Maciej Witkowski <maciekwitkowski322@gmail.com>
2018-08-04 22:27:53 +0200

    Neuron class v1

--- NeuralNet/neuron.js
+++ NeuralNet/neuron.js
@@ -1,8 +1,26 @@
-class neuron {
-
-    constructor(inputs, weights, activFun) {
-        this.inputs = inputs;
-        this.weights = weights;
+class Neuron {
+    /**
+     * @param {Function} _actFunct Activation function for this layer
+     * @param {Number} _weightCount Number of how many inputs there will be
+     * @param {Function} _initCallback Initialization callback for weights
+     */
+    constructor(_actFunct, _weightCount, _initCallback) {
+        this.weights = new Array(_weightCount).fill().map(_initCallback) ;
+        this.actFunct = _actFunct;
+        this.bias = 0;
     }
+    sum() {
+        let finalSum = 0;
+        this.inputs.forEach((input, index) => {
+            finalSum += input * this.weights[index];
+        });
 
+        finalSum += this.bias;
+        return finalSum;
+    }
+    activate(_inputs) {
+        this.inputs = _inputs;
+
+        return this.actFunct(this.sum());
+    }
 }
