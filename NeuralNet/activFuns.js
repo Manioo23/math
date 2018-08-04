f0db7ba2537c7e1ab305a3371285620cf758f713
karylsienn <niewiadomski.k@wp.pl>
2018-08-04 20:42:25 +0200

    activFuns neuron

---
+++ NeuralNet/activFuns.js
@@ -0,0 +1,5 @@
+var activFuns = [
+    "perceptron",
+    "tanh",
+    "sigmoid"
+]
---
+++ NeuralNet/neuron.js
@@ -0,0 +1,8 @@
+class neuron {
+
+    constructor(inputs, weights, activFun) {
+        this.inputs = inputs;
+        this.weights = weights;
+    }
+
+}
