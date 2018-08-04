3b274aa42d25cb5f706c216f4f20cca2be7e8f46
karylsienn <niewiadomski.k@wp.pl>
2018-08-04 23:30:43 +0200

    Net init with neurons

--- NeuralNet/net.js
+++ NeuralNet/net.js
@@ -1,3 +1,47 @@
-class net {
+class Net {
+
+    constructor(_sizes, _activations) {
+        if (typeof(_activations) == "string") {
+            _activations = Array(_sizes.length).fill(_activations);
+        }
+        if (_sizes.length != _activations.length) {
+            throw new Error("lengths don't match!");
+        } else {
+            this.numLayers = _sizes.length;
+            this.sizes = _sizes;
+            this.activations = _activations;
+            this.createNeurons();
+        }
+    }
+
+    createNeurons() {
+        let neurons = Array(this.numLayers);
+
+        // Initialize inputs separately
+        let layerSize = this.sizes[0]
+        let actFun = this.activations[0]
+        neurons[0] = Array(layerSize);
+        for(let j = 0; j < layerSize; j++) {
+            neurons[0][j] = new Neuron(actFun, 1, ones)
+        }
+
+        // Initialize all the other layers in second place.
+        // Counting starts at 1 - first layer.
+        for (let i = 1; i < this.numLayers; i++) {
+            layerSize = this.sizes[i]
+            let previousLayerSize = this.sizes[i - 1]
+            actFun = this.activations[i]
+            neurons[i] = Array(layerSize);
+            for(let j = 0; j < layerSize; j++) {
+                neurons[i][j] = new Neuron(actFun, previousLayerSize, Math.random);
+            }
+        }
+
+        this.neurons = neurons;
+    }
+
+
+
+
 
 }
---
+++ NeuralNet/weight_init.js
@@ -0,0 +1,3 @@
+function ones() {
+    return 1;
+}
